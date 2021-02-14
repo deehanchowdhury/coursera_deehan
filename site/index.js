8310cd275446809e89b4fb3433ddccc21b31a3c5
deehan <deehanchowdhury@gmail.com>
2021-02-14 20:38:30 +0600

    edition : 14th feb

---
+++ site/index.js
@@ -0,0 +1,8 @@
+//document.write("<h1>This is my first webpage!</h1><h2>Here's a simple recitation by me..</h2><iframe width='560' height='315' src='https://www.youtube.com/embed/y63ll5aBA_o' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+var num1 = prompt("Enter 1st number");
+var num2 = prompt("Enter 2nd number");
+num1 = parseInt(num1,10);
+num2 = parseInt(num2,10);
+var sum = num1+num2;
+document.write("The first number = "+num1+" The second number = "+num2+ " The sum = "+sum);
+document.write("<h1>This is my first webpage!</h1><h2>Here's a simple recitation by me..</h2><iframe width='560' height='315' src='https://www.youtube.com/embed/y63ll5aBA_o' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
